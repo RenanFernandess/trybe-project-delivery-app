1b5995e862e276bf2f85aaaefaecded2330e9c72
Gabriel Goncalves <gabrielraeder@outlook.com.br>
2023-02-27 17:55:59 -0300

    refactor: removing console logs

--- back-end/src/routes/Login.router.js
+++ back-end/src/routes/Login.router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => new LoginController(req, res, next).getAll());
 router.get('/:id', (req, res, next) => new LoginController(req, res, next).getById());
-router.post('/', (req, res, next) => new LoginController(req, res, next).create());
+router.post('/register', (req, res, next) => new LoginController(req, res, next).create());
 router.put('/:id', (req, res, next) => new LoginController(req, res, next).update());
 router.delete('/:id', (req, res, next) => new LoginController(req, res, next).remove());
 
