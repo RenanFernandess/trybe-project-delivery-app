bf85f2f289b1e15444ae646ba96295699191378d
Gabriel Goncalves <gabrielraeder@outlook.com.br>
2023-02-27 16:13:31 -0300

    feat: migrations and seeders

---
+++ back-end/src/database/migrations/20230227182408-sales.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('sales', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      seller_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      total_price: {
+        allowNull: false,
+        type: Sequelize.FLOAT(9, 2),
+      },
+      delivery_address: {
+        allowNull: false,
+        type: Sequelize.STRING(100),
+      },
+      delivery_number: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      sale_date: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('sales');
+  }
+};
