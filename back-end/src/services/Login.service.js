dd830522ddf47ff9868ccb18939a6c73e9c3bb12
Rubens Deola <rdeola@gmail.com>
2023-02-28 11:12:34 -0300

    feat: adicionada criação de usuário à rota /login/register

--- back-end/src/services/Login.service.js
+++ back-end/src/services/Login.service.js
@@ -1,7 +1,7 @@
 const AbstractService = require('./Abstract.service');
 const { User } = require('../database/models');
 const HttpException = require('../utils/HttpException');
-const { hashMd5Compare } = require('../utils/Hash');
+const { hashMd5Compare, hashMd5Encrypt } = require('../utils/Hash');
 const { getToken } = require('../utils/Token');
 
 class LoginService extends AbstractService {
@@ -26,6 +26,16 @@ class LoginService extends AbstractService {
     }
     const { password: _, ...userWithoutPassword } = result.dataValues;
     return { ...userWithoutPassword, token: getToken(userWithoutPassword) };
+  }
+
+  async create(user) {
+    const { email, password } = user;
+    const result = await this.getByEmail(email);
+    if (result) throw new HttpException(409, 'User already exists');
+    const newUser = await super.create({ ...user, password: hashMd5Encrypt(password) });
+    console.log(newUser);
+    const { password: _, ...userWithoutPassword } = newUser.dataValues;
+    return userWithoutPassword;
   }
 }
 
