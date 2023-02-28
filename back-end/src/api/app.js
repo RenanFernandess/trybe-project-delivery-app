e0f28fe54ee540fb999a6aecfedb276ef5718cfa
Gabriel Goncalves <gabrielraeder@outlook.com.br>
2023-02-28 14:39:54 -0300

    feat: images folder

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -5,6 +5,7 @@ const router = require('../routes');
 
 const app = express();
 app.use(express.json());
+app.use('/images', express.static(`${__dirname}/images`));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(router);
