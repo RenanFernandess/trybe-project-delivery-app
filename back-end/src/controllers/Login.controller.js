52b28705b80afc6f2ca77afd1eda69eb48f68d5c
Rubens Deola <rdeola@gmail.com>
2023-02-28 10:48:24 -0300

    feat: adicionados validação de senha e retorno de token

--- back-end/src/controllers/Login.controller.js
+++ back-end/src/controllers/Login.controller.js
@@ -3,12 +3,23 @@ const LoginService = require('../services/Login.service');
 
 class LoginController extends AbstractController {
   constructor(req, res, next) {
+    const loginService = new LoginService();
     super(
-      new LoginService(),
-      req, 
-      res, 
+      loginService,
+      req,
+      res,
       next,
     );
+    this.loginService = loginService;
+  }
+
+  async login() {
+    try {
+      const newObj = await this.loginService.login(this.req.body);
+      return this.res.status(200).json(newObj);
+    } catch (error) {
+      this.next(error);
+    }
   }
 }
 
--- back-end/src/services/Login.service.js
+++ back-end/src/services/Login.service.js
@@ -1,9 +1,31 @@
 const AbstractService = require('./Abstract.service');
 const { User } = require('../database/models');
+const HttpException = require('../utils/HttpException');
+const { hashMd5Compare } = require('../utils/Hash');
+const { getToken } = require('../utils/Token');
 
 class LoginService extends AbstractService {
   constructor() {
     super(User);
+    this.user = User;
+  }
+
+  async getByEmail(email) {
+    const result = await this.user.findOne({
+      where: { email },
+    });
+    return result;
+  }
+
+  async login(user) {
+    const { email, password } = user;
+    const result = await this.getByEmail(email);
+    if (!result) throw new HttpException(404, 'Not Found');
+    if (!hashMd5Compare(password, result.password)) {
+      throw new HttpException(404, 'email or password incorrect');
+    }
+    const { password: _, ...userWithoutPassword } = result.dataValues;
+    return { ...userWithoutPassword, token: getToken(userWithoutPassword) };
   }
 }
 
