a17f0f8dc8420121e79ab5faadd8d1710b16b960
Renan Fernandes <renzinestuods@gmail.com>
2023-02-28 18:27:27 -0300

    :sparkles: feat: Adiciona a funcionalidade de salvar o token no local storage

--- front-end/src/context/user/UserProvider.js
+++ front-end/src/context/user/UserProvider.js
@@ -1,6 +1,7 @@
 import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import userContext from './userContext';
+import { saveToken } from '../../utils';
 
 export default function UserProvider({ children }) {
   const [state, setState] = useState({
@@ -9,8 +10,11 @@ export default function UserProvider({ children }) {
     token: '',
     message: null,
   });
+  const { token } = state;
 
   const setUser = (user) => { setState((prevState) => ({ ...prevState, ...user })); };
+
+  useMemo(() => { saveToken(token); }, [token]);
 
   const contextType = useMemo(() => ({ ...state, setUser }), [state]);
 
