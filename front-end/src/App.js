8c50abec5c14f5313febdddb20756de4e979e56a
Renan Fernandes <renzinestuods@gmail.com>
2023-02-28 16:38:17 -0300

    :boom: fix: Configura as rotas para funcionar com react-router-dom na versão 5

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Login from './pages/Login';
+import Login from './pages';
 
 function App() {
   return (
     <main>
-      <Routes>
-        <Route path="/" element={ <Navigate to="/login" replace /> } />
-        <Route path="/login" element={ <Login /> } />
-      </Routes>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={ () => <Redirect to="/login" /> }
+        />
+        <Route exact path="/login" component={ Login } />
+      </Switch>
     </main>
   );
 }
