54f0d7f469187d3af9bcec7fd5369ad119190e8f
Renan Fernandes <renzinestuods@gmail.com>
2023-03-01 20:00:57 -0300

    :sparkles: feat: Cria a estrutura da pagina de produtos

---
+++ front-end/src/pages/Products/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from '../../components';
+
+export default function Products() {
+  return (
+    <div>
+      <NavBar />
+      Products
+    </div>
+  );
+}
--- front-end/src/pages/index.js
+++ front-end/src/pages/index.js
@@ -1,3 +1,7 @@
 import Login from './Login';
+import Register from './Register';
+import CustomerOrder from './CustomerOrder';
+import Products from './Products';
 
 export default Login;
+export { Register, CustomerOrder, Products };
