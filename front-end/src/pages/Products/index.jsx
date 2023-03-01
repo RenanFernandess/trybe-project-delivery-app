d3dcc602bff26978b07ee047677a410d8dae8630
Renan Fernandes <renzinestuods@gmail.com>
2023-03-01 20:35:58 -0300

    :sparkles: faet: Adiciona uma tela de loading e faz a rquisição dos produtos

--- front-end/src/pages/Products/index.jsx
+++ front-end/src/pages/Products/index.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from '../../components';
+import { getAPI } from '../../utils';
 
 export default function Products() {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getAPI('/products', (data) => {
+      setProducts(data);
+      setLoading(false);
+    });
+  });
+
   return (
     <div>
       <NavBar />
-      Products
+      { loading
+        ? <p>Loading...</p>
+        : (
+          <section>
+            { products.map((product) => <p key={ product.id }>{ product.name }</p>) }
+          </section>
+        ) }
     </div>
   );
 }
