7b301cc035cd5dc0d305ab324622e3d17c855870
Renan Fernandes <renzinestuods@gmail.com>
2023-03-01 15:29:16 -0300

    :sparkles: feat: Configura o CORS

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,12 @@
 // app
 const express = require('express');
 const ErrorHandler = require('../middlewares/error.middleware');
+const cors = require('cors');
 const router = require('../routes');
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use('/images', express.static(`${__dirname}/../images`));
 
