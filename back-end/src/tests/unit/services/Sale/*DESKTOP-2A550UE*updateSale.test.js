f9aa4ec627870e2bf8b27714ac246c2bfbc92ca0
Gabriel Goncalves <gabrielraeder@outlook.com.br>
2023-03-01 12:59:48 -0300

    test: SaleService update sale

---
+++ back-end/src/tests/unit/services/Sale/*DESKTOP-2A550UE*updateSale.test.js
@@ -0,0 +1,30 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const { Sale } = require('../../../../database/models');
+const SaleService = require('../../../../services/Sale.service');
+const { findOneSaleMock, getByIdMock, createdSaleMock } = require('./mocks/sales.mock')
+
+describe('Tests SaleService update function', function () {
+  beforeEach(sinon.restore);
+  it('Successfully Updates status', async function () {
+    sinon.stub(Sale, 'update').resolves([1]);
+
+    const saleService = new SaleService();
+    sinon.stub(saleService, 'getById').resolves(createdSaleMock);
+    const result = await saleService.update(1, { status: 'Entregue' });
+
+    expect(result).to.deep.equal({...createdSaleMock });
+  });
+
+  it('Fails to Update status', async function () {
+    sinon.stub(Sale, 'update').resolves([0]);
+
+    const saleService = new SaleService();
+    try {
+      await saleService.update(1, { status: 'Entregue' });
+    } catch (error) {
+      expect(error.message).to.equal('Not Found');
+    }
+  });
+});
