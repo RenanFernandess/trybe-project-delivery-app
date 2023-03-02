35865552bd6bd7cfa4b425f7d2e207259b0a0bb4
Renan Fernandes <renzinestuods@gmail.com>
2023-03-02 20:50:47 -0300

    :sparkles: feat: Cria a pagina Checkout

--- front-end/src/pages/index.js
+++ front-end/src/pages/index.js
@@ -2,6 +2,7 @@ import Login from './Login';
 import Register from './Register';
 import CustomerOrder from './CustomerOrder';
 import Products from './Products';
+import Checkout from './Checkout';
 
 export default Login;
-export { Register, CustomerOrder, Products };
+export { Register, CustomerOrder, Products, Checkout };
