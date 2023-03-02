0dcf74facb14da556725630c1d0aee90029cadfa
Rubens Deola <rdeola@gmail.com>
2023-03-02 16:04:54 -0300

    fix: changed product price type from Float to Decimal

--- front-end/src/pages/Products/index.jsx
+++ front-end/src/pages/Products/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import NavBar from '../../components';
 import { getAPI } from '../../utils';
-import Login from '../index';
 
 export default function Products() {
   const [/* loading */, setLoading] = useState(true);
