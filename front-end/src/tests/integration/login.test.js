3bbab7eaab851562e13ecc8c2261bb5e885cad0e
Renan Fernandes <renzinestuods@gmail.com>
2023-03-02 14:35:44 -0300

    :white_check_mark: test: O botão deve estar desativado com um email no formato válido e senha com menos de 6 caracteres

--- front-end/src/tests/integration/login.test.js
+++ front-end/src/tests/integration/login.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import App from '../../App';
 import renderWithRouter from '../utils/renderWithRouter';
 
@@ -38,6 +38,21 @@ describe('Testa a tela de Login', () => {
 
           userEvent.type(email, 'test@gmail');
           userEvent.type(password, '123456');
+
+          expect(login).toBeDisabled();
+        },
+      );
+
+      it(
+        `O botão deve estar desativado com um email no formato válido 
+        e senha com menos de 6 caracteres.`,
+        () => {
+          const email = screen.getByRole('textbox', { name: /login/i });
+          const password = screen.getByLabelText(/senha/i);
+          const login = screen.getByRole('button', { name: /login/i });
+
+          userEvent.type(email, 'zebirita@email.com');
+          userEvent.type(password, '12345');
 
           expect(login).toBeDisabled();
         },
