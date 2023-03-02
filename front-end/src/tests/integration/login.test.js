45eadfc508a5e9479b7091d264b332a02b518a30
Renan Fernandes <renzinestuods@gmail.com>
2023-03-02 14:28:00 -0300

    :white_check_mark: test: Testa se o botão de LOGIN ativa somente com email e senha no formato válido: O botão deve inicia desativado

--- front-end/src/tests/integration/login.test.js
+++ front-end/src/tests/integration/login.test.js
@@ -5,9 +5,11 @@ import App from '../../App';
 import renderWithRouter from '../utils/renderWithRouter';
 
 describe('Testa a tela de Login', () => {
-  it('Verifica se possui um formulario de login', () => {
+  beforeEach(() => {
     renderWithRouter(<App />);
+  });
 
+  it('Verifica se possui um formulario de login', () => {
     const email = screen.getByRole('textbox', { name: /login/i });
     const password = screen.getByLabelText(/senha/i);
     const login = screen.getByRole('button', { name: /login/i });
@@ -18,4 +20,14 @@ describe('Testa a tela de Login', () => {
     expect(login).toBeInTheDocument();
     expect(register).toBeInTheDocument();
   });
+
+  describe(
+    'Testa se o botão de LOGIN ativa somente com email e senha no formato válido:',
+    () => {
+      it('O botão deve inicia desativado', () => {
+        const login = screen.getByRole('button', { name: /login/i });
+        expect(login).toBeDisabled();
+      });
+    },
+  );
 });
