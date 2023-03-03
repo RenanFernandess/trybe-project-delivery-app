efd697ff30c4a8bcd7269b99a83871336e2019e0
Renan Fernandes <renzinestuods@gmail.com>
2023-03-03 15:39:30 -0300

    :bug: fix: Resolve o bug do stado inicial do usuario ser salvo no local storage

--- front-end/src/context/user/UserProvider.js
+++ front-end/src/context/user/UserProvider.js
@@ -16,9 +16,11 @@ export default function UserProvider({ children }) {
 
   const setUser = (user) => { setState((prevState) => ({ ...prevState, ...user })); };
 
-  const resetUser = () => setState(initialState);
+  const resetUser = async () => setState(initialState);
 
-  useMemo(() => { saveUser({ token, name, role, email }); }, [token, name, role, email]);
+  useMemo(() => {
+    if (token) saveUser({ token, name, role, email });
+  }, [token, name, role, email]);
 
   const contextType = useMemo(() => ({ ...state, setUser, resetUser }), [state]);
 
