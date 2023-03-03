9883d258cbd5a6e983017ea07d0a55de2d37835a
Rubens Deola <rdeola@gmail.com>
2023-03-03 15:03:48 -0300

    refactor: ajustes nos cards e customer products

--- back-end/src/utils/Token.js
+++ back-end/src/utils/Token.js
@@ -3,7 +3,7 @@ const readFile = require('fs');
 
 const getToken = (payload) => {
   console.log('dirname', __dirname);
-  const secret = readFile.readFileSync(`../back-end/jwt.evaluation.key`, { encoding: 'utf-8' });
+  const secret = readFile.readFileSync('../back-end/jwt.evaluation.key', { encoding: 'utf-8' });
   // const jwtConfig = { algorithm: 'HS256', expiresIn: '7d' };
   return jwt.sign({ payload }, secret);
 };
