dd778fba6dda037137c375918fa47245e8091f28
Renan Fernandes <renzinestuods@gmail.com>
2023-03-03 16:02:19 -0300

    :sparkles: feat: Cria o CartProvider

---
+++ front-end/src/context/cart/CartProvider.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import cartContext from './cartContext';
+
+export default function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  const contextType = useMemo(() => ({ ...cart, setCart }), [cart]);
+
+  return (
+    <cartContext.Provider value={ contextType }>
+      { children }
+    </cartContext.Provider>
+  );
+}
+
+CartProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
