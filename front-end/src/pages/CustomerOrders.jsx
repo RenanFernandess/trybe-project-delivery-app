6aa294f6334028d6f5df930877cd07fb6440957b
Gabriel Goncalves <gabrielraeder@outlook.com.br>
2023-03-06 17:16:31 -0300

    fix: seller order details

--- front-end/src/pages/CustomerOrders.jsx
+++ front-end/src/pages/CustomerOrders.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import NavBar from '../components/navBar';
 import OrderCard from '../components/orderCard';
 import { getAPI } from '../utils';
@@ -7,11 +7,13 @@ import userContext from '../context';
 // import getUserId from '../utils/getUserId';
 
 export default function CustomerOrders() {
+  const { location: { pathname } } = useHistory();
   const { id } = useContext(userContext);
   const [orders, setOrders] = useState([]);
   // const [id, setId] = useState('');
 
-  const route = `/sales/user/${id}'`;
+  const route = pathname
+    .includes('customer') ? `/sales/user/${id}'` : `/sales/seller/${id}'`;
 
   // const getId = async () => {
   //   const userId = await getUserId();
@@ -35,9 +37,16 @@ export default function CustomerOrders() {
       <NavBar name="teste" route="customer" />
       {
         orders.length > 0 && orders.map((order, index) => (
-          <Link to={ `/customer/orders/${order.id}` } key={ order.id }>
+          <Link
+            to={ pathname
+              .includes('customer')
+              ? `/customer/orders/${order.id}`
+              : `/seller/orders/${order.id}` }
+            key={ order.id }
+          >
             <OrderCard
-              route="customer_orders"
+              route={ pathname
+                .includes('customer') ? 'customer_orders' : 'seller_orders' }
               order={ order }
               index={ index }
             />
