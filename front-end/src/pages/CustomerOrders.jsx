18fdf5ccab4e5a2aaf796dff6efb95af9c2d1e2f
Renan Fernandes <renzinestuods@gmail.com>
2023-03-13 18:38:05 -0300

    :lipstick: feat: Estiliza a o card de pedidos (orderCard)

--- front-end/src/pages/CustomerOrders.jsx
+++ front-end/src/pages/CustomerOrders.jsx
@@ -1,50 +1,58 @@
-import { useState, useEffect, useContext } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useState, useEffect, useContext, useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
 import NavBar from '../components/navBar';
 import OrderCard from '../components/orderCard';
 import { getAPI } from '../utils';
 import userContext from '../context';
-// import getUserId from '../utils/getUserId';
+import { Loading } from '../components';
 
 export default function CustomerOrders() {
   const { location: { pathname } } = useHistory();
   const { id } = useContext(userContext);
+  const [loadin, setLoadin] = useState(true);
   const [orders, setOrders] = useState([]);
 
   const route = pathname
     .includes('customer') ? `/sales/user/${id}'` : `/sales/seller/${id}'`;
 
-  const getOrders = async () => {
+  const getOrders = useCallback(async () => {
     await getAPI(
       route,
-      (data) => setOrders(data),
+      (data) => {
+        setOrders(data);
+        setLoadin(false);
+      },
     );
-  };
+  }, [route]);
 
   useEffect(() => {
     getOrders();
-  }, []);
+  }, [getOrders]);
 
   return (
     <div>
-      <NavBar name="teste" route="customer" />
+      <NavBar
+        name="teste"
+        route={
+          pathname
+            .includes('customer') ? 'customer' : 'seller'
+        }
+      />
       {
-        orders.length > 0 && orders.map((order, index) => (
-          <Link
-            to={ pathname
-              .includes('customer')
-              ? `/customer/orders/${order.id}`
-              : `/seller/orders/${order.id}` }
-            key={ order.id }
-          >
-            <OrderCard
-              route={ pathname
-                .includes('customer') ? 'customer_orders' : 'seller_orders' }
-              order={ order }
-              index={ index }
-            />
-          </Link>
-        ))
+        loadin ? <Loading /> : (
+          <section className="c-body c-list-card">
+            {
+              orders.length > 0 && orders.map((order, index) => (
+                <OrderCard
+                  key={ order.id }
+                  pathname={ pathname }
+                  order={ order }
+                  index={ index }
+                />
+              ))
+            }
+          </section>
+        )
       }
     </div>
   );
